1569c56206601b2102b1d79f0228ccf74480ac85
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2025-06-05 14:15:40 +0000

    lista-rev04.py

--- Aula_12/lista-rev04.py
+++ Aula_12/lista-rev04.py
@@ -41,4 +41,45 @@ class ViagemUI:
 
 ViagemUI.main()
         
+# QUESTÃO 02
+class Pais: 
+    def __init__(self):
+        self.__nome = ''
+        self.__pop = 0
+        self.__area = 0.0
+    def set_nome(self, pais):
+        if pais == '': raise ValueError('Necessário informar o nome do país')
+        self.__nome = pais
+    def set_populacao(self, hab):
+        if hab >= 0: self.__pop = hab
+        else: ValueError()
+    def set_area(self, km):
+        if km >= 0: self.__area = km
+        else: ValueError()
+    def get_nome(self): return self.__nome
+    def get_populacao(self): return self.__pop
+    def get_area(self): return self.__area
+
+    def densidade(self): return self.__pop / self.__area
     
+class ParcialUI: 
+    @staticmethod
+    def menu():
+        op_2 = int(input('1- Calcular, 2- Fim'))
+        return op_2
+    @staticmethod
+    def main():
+        op_2 = 0
+        while op_2 != 2:
+            op_2 = ParcialUI.menu()
+            if op_2 == 1: ParcialUI.calculo()
+    @staticmethod
+    def calculo():
+        x = Pais()
+        x.set_nome(input('Informe o nome do país: '))
+        x.set_populacao(int(input('Informe o nº de habitantes do país: ')))
+        x.set_area(float(input('informe a área em km2: ')))
+        print(f'A densidade demográfica é de {x.densidade()} km2')
+
+ParcialUI.main()
+
