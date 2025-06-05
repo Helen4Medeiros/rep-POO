2663661360eb95cc4fd1259994a8f4dbc3a66386
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2025-06-05 12:30:45 +0000

    lista-poo02

--- Aula_10/lista-poo02.py
+++ Aula_10/lista-poo02.py
@@ -31,6 +31,35 @@ class Velocidade:
         return self.__d / self.__t
 
 # QUESTÃO 03: CONTA BANCÁRIA
+class Banco:
+    def __init__(self):
+        self.__nome = ""
+        self.__conta = ""
+        self.__saldo = 0.00
+    def set_conta(self, id):
+        if id == "": raise ValueError("ID inválido")
+        self.__conta = id
+    def set_titular(self, titular):
+        if titular == "": raise ValueError
+        self.__nome = titular
+    def set_saldo(self, saldo):
+        if not saldo >= 0 or saldo == "": raise ValueError ('Saldo Inválido')
+        self.__saldo = saldo
+    def get_conta(self): return self.__conta
+    def get_titular(self): return self.__nome
+    def get_saldo(self): return self.__saldo
+    
+    def deposito(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            print(f'Depósito de R$ {valor:.2f} realizado com sucesso.')
+        else: print('Não foi feito nenhum valor p/ depósito.')
+    def saque(self, valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+            print(f'Saque de R$ {valor:.2f} realizado com sucesso.')
+        elif valor > self.__saldo: print('Saldo insuficiente.')
+        else: print('Valor de saque deve ser positivo.')
 
 # QUESTÃO 04: ENTRADA DO CINEMA 
 
@@ -47,6 +76,7 @@ class UI:
             op = UI.menu()
             if op == 1: UI.circulo()
             if op == 2: UI.velocidade()
+            if op == 3: UI.banco()
     @staticmethod
     def circulo():
         x = Circulo()
@@ -58,5 +88,15 @@ class UI:
         x.set_d(float(input('Informe a distância percorrida: ')))
         x.set_t(float(input('Informe o tempo: ')))
         print(f'A velocidade média percorrida foi de {x.velocidade()}km/h')
+    @staticmethod
+    def banco():
+        x = Banco()
+        x.set_titular(input("Qual seu nome?"))
+        x.set_conta(input("Qual o seu id? "))
+        x.set_saldo(float(input("Qual o seu saldo inicial? R$")))
+        x.deposito(float(input("Faça seu primeiro depósito! => R$")))
+        print(f"[!]          Titular {x.get_titular()} fez um depósito. Saldo atual = {x.get_saldo():.2f} reais")
+        x.saque(float(input("Faça o seu primeiro saque! => ")))
+        print(f"[!]          Titular {x.get_titular()} fez um saque. Saldo atual = {x.get_saldo():.2f} reais")
 
 UI.main()
