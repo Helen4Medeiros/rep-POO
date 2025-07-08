476a847b83891ecf6a308498cb5dd3f893134307
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2025-07-08 14:49:04 +0000

    lista_poo05

--- Aula_13/lista_poo05.py
+++ Aula_13/lista_poo05.py
@@ -1,1 +1,16 @@
-# TERÇA-FEIRA
+# 1)
+
+class Paciente: 
+    def __init__(self, n, c, t, nas):
+        self.__nome = n
+        self.__cpf = c
+        self.__telefone = t
+        self.__nascimento = nas
+        
+    def get_nome(self): return self.__nome
+    def get_cpf(self): return self.__cpf
+    def get_telefone(self): return self.__telefone
+    def get_nascimento(self): return self.__nascimento
+
+
+        
