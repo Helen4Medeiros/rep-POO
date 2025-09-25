d16348306e1f582463b4c61bc1afeff8624db147
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2025-09-25 12:30:47 +0000

    sistema-agendamento

--- Aula_19/Sistema-Agendamento/views.py
+++ Aula_19/Sistema-Agendamento/views.py
@@ -30,6 +30,14 @@ class View:
         ServicoDAO.excluir(servico)
 
     # horario
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.inserir(c)
+    def horario_listar():
+        return HorarioDAO.listar()
     def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
         c = Horario(id, data)
         c.set_confirmado(confirmado)
