87336d52272f6025365ec3d30b80c1476210618d
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2025-10-30 13:03:29 +0000

    lista-poo14 - dao.py

--- lista-poo12-13/agendamento/models/cliente.py
+++ lista-poo12-13/agendamento/models/cliente.py
@@ -36,69 +36,22 @@ class Cliente:
         return Cliente(dic["id"], dic["nome"], dic["email"], dic["fone"], dic["senha"])
     
 import json
+from models.dao import DAO
 
-class ClienteDAO():
-    __objetos = []
-    __objetos_listados = []
-    @classmethod
-    def inserir(cls, obj):
-        cls.abrir()
-        id = 0
-        for aux in cls.__objetos:
-            if aux.get_id() > id: id = aux.get_id()
-        obj.set_id(id + 1)
-        cls.__objetos.append(obj)
-        cls.salvar()
-
-    @classmethod
-    def listar_clientes(cls):
-        cls.abrir()
-        dic = []
-        for obj in cls.__objetos:
-            dic.append({"id": obj.get_id(), "nome": obj.get_nome(), "email": obj.get_email(), "fone": obj.get_fone()})
-        return dic
-
-    @classmethod
-    def listar(cls):
-        cls.abrir()
-        return cls.__objetos
-    
-    @classmethod
-    def listar_id(cls, id):
-        cls.abrir()
-        for obj in cls.__objetos:
-            if obj.get_id() == id: return obj
-        return None
-    
-    @classmethod
-    def atualizar(cls, obj):
-        aux = cls.listar_id(obj.get_id())
-        if aux != None:
-            cls.__objetos.remove(aux)
-            cls.__objetos.append(obj)
-            cls.salvar()
-
-    @classmethod
-    def excluir(cls, obj):
-        aux = cls.listar_id(obj.get_id())
-        if aux != None:
-            cls.__objetos.remove(aux)
-            cls.salvar()
-
+class ClienteDAO(DAO):
     @classmethod
     def abrir(cls):
-        cls.__objetos = []
+        cls._objetos = []
         try:
             with open("clientes.json", mode="r") as arquivo:
                 list_dic = json.load(arquivo)
                 for dic in list_dic:
                     obj = Cliente.from_json(dic)
-                    cls.__objetos.append(obj)
+                    cls._objetos.append(obj)
         except FileNotFoundError:
             pass 
-
     @classmethod
     def salvar(cls):
         with open("clientes.json", mode="w") as arquivo:
-            json.dump(cls.__objetos, arquivo, default = Cliente.to_json)
+            json.dump(cls._objetos, arquivo, default = Cliente.to_json)
                 
